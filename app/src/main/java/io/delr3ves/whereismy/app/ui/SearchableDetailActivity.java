9a0984d22efc988b4acb6c488e782e0afce18636
Sergio Arroyo <sergio@emaginalabs.com>
2015-04-10 00:28:43 +0200

    confirm searchables deletion

--- app/src/main/java/io/delr3ves/whereismy/app/ui/SearchableDetailActivity.java
+++ app/src/main/java/io/delr3ves/whereismy/app/ui/SearchableDetailActivity.java
@@ -28,11 +28,4 @@ public class SearchableDetailActivity extends ActionBarActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_searchable_detail, menu);
-        return true;
-    }
-
 }
--- app/src/main/java/io/delr3ves/whereismy/app/ui/SearchableDetailFragment.java
+++ app/src/main/java/io/delr3ves/whereismy/app/ui/SearchableDetailFragment.java
@@ -1,12 +1,10 @@
 package io.delr3ves.whereismy.app.ui;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
+import android.view.*;
 import io.delr3ves.whereismy.app.R;
 import io.delr3ves.whereismy.app.WhereismyApplication;
 import io.delr3ves.whereismy.app.business.model.Searchable;
@@ -31,7 +29,13 @@ public class SearchableDetailFragment extends Fragment {
         searchable = (Searchable) getActivity().getIntent().getSerializableExtra(
                 SearchableDetailActivity.SEARCHABLE_EXTRA);
 
+        setHasOptionsMenu(true);
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_searchable_detail, menu);
     }
 
     @Override
@@ -39,11 +43,30 @@ public class SearchableDetailFragment extends Fragment {
         int id = item.getItemId();
 
         if (id == R.id.action_delete_searchable) {
-            searchableDao.delete(searchable);
-            getActivity().finish();
+            conformableRemove();
             return true;
         }
         return super.onOptionsItemSelected(item);
+
+    }
+
+    public void conformableRemove() {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE: // Yes button clicked Toast.makeText(AlertDialogActivity.this, "Yes Clicked", Toast.LENGTH_LONG).show(); break; case DialogInterface.BUTTON_NEGATIVE: // No button clicked // do nothing Toast.makeText(AlertDialogActivity.this, "No Clicked", Toast.LENGTH_LONG).show(); break; } } }; AlertDialog.Builder builder = new AlertDialog.Builder(this); builder.setMessage("Are you sure?") .setPositiveButton("Yes", dialogClickListener) .setNegativeButton("No", dialogClickListener).show(); }
+                        searchableDao.delete(searchable);
+                        getActivity().finish();
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE: // No button clicked // do nothing Toast.makeText(AlertDialogActivity.this, "No Clicked", Toast.LENGTH_LONG).show(); break; } }
+                        break;
+                }
+            }
+        };
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(getString(R.string.confirm_delete_searchable))
+                .setPositiveButton(getString(R.string.confirm_delete_searchable_positive), dialogClickListener)
+                .setNegativeButton(getString(R.string.confirm_delete_searchable_negative), dialogClickListener).show();
 
     }
 }
